84ea06ed4def74c03aa0cac0c3ccbdfe2fac6241
jvalero <jvalero84@gmail.com>
2022-08-24 18:54:31 +0200

    Fine tuning oracle test

--- FlightSurety/test/oracles.js
+++ FlightSurety/test/oracles.js
@@ -21,12 +21,12 @@ contract('Oracles', async (accounts) => {
 
 
   it('can register oracles', async () => {
-    
+
     // ARRANGE
     let fee = await config.flightSuretyApp.REGISTRATION_FEE.call();
 
     // ACT
-    for(let a=1; a<TEST_ORACLES_COUNT; a++) {      
+    for(let a=1; a<TEST_ORACLES_COUNT; a++) {
       await config.flightSuretyApp.registerOracle({ from: accounts[a], value: fee });
       let result = await config.flightSuretyApp.getMyIndexes.call({from: accounts[a]});
       console.log(`Oracle Registered: ${result[0]}, ${result[1]}, ${result[2]}`);
@@ -34,10 +34,12 @@ contract('Oracles', async (accounts) => {
   });
 
   it('can request flight status', async () => {
-    
+
+    const STATUS_CODE_ON_TIME = 10;
+
     // ARRANGE
     let flight = 'ND1309'; // Course number
-    let timestamp = Math.floor(Date.now() / 1000);
+    let timestamp = 0;
 
     // Submit a request for oracles to get status information for a flight
     await config.flightSuretyApp.fetchFlightStatus(config.firstAirline, flight, timestamp);
@@ -56,11 +58,12 @@ contract('Oracles', async (accounts) => {
         try {
           // Submit a response...it will only be accepted if there is an Index match
           await config.flightSuretyApp.submitOracleResponse(oracleIndexes[idx], config.firstAirline, flight, timestamp, STATUS_CODE_ON_TIME, { from: accounts[a] });
+          console.log('\nIndex match!', idx, oracleIndexes[idx].toNumber(), flight, timestamp);
 
         }
         catch(e) {
           // Enable this when debugging
-           console.log('\nError', idx, oracleIndexes[idx].toNumber(), flight, timestamp);
+          //console.log('\nError', idx, oracleIndexes[idx].toNumber(), flight, timestamp);
         }
 
       }
@@ -70,5 +73,5 @@ contract('Oracles', async (accounts) => {
   });
 
 
- 
+
 });
